5361ed8cf57beff30fa38b4c1f95c8ac8f20feaf
unknown <Seth Marinello@.(none)>
2011-05-08 21:57:52 -0700

    added a linux source tree with cmakelist files.

---
+++ ags_linux/Engine/CMakeFiles/ags.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/user/Downloads/ags/Engine/almp3.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/almp3.c.o"
+  "/home/user/Downloads/ags/Engine/alogg.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/alogg.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/aastr-0.1.1/aastr.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/aastr-0.1.1/aastr.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/aastr-0.1.1/aautil.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/aastr-0.1.1/aautil.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/allegro-4.2.2-agspatch/file.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/allegro-4.2.2-agspatch/file.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/common.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/common.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/dct64_i386.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/dct64_i386.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/decode_i386.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/decode_i386.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/interface.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/interface.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/layer2.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/layer2.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/layer3.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/layer3.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/almp3-2.0.5/decoder/tabinit.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/almp3-2.0.5/decoder/tabinit.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/apegcommon.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/apegcommon.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/audio.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/audio.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/dct64.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/dct64.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/decode_1to1.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/decode_1to1.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/decode_2to1.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/decode_2to1.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/decode_4to1.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/decode_4to1.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/layer1.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/layer1.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/layer2.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/layer2.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/layer3.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/layer3.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/mpg123.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/mpg123.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/readers.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/readers.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/tabinit.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/tabinit.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/audio/vbrhead.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/audio/vbrhead.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/display.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/display.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/getbits.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/getbits.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/getblk.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/getblk.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/gethdr.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/gethdr.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/getpic.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/getpic.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/idct.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/idct.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/motion.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/motion.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/mpeg1dec.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/mpeg1dec.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/ogg.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/ogg.c.o"
+  "/home/user/Downloads/ags/Engine/libsrc/apeg-1.2.1/recon.c" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/apeg-1.2.1/recon.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Downloads/ags/Common/Clib32.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/Clib32.cpp.o"
+  "/home/user/Downloads/ags/Common/compress.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/compress.cpp.o"
+  "/home/user/Downloads/ags/Common/cscommon.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/cscommon.cpp.o"
+  "/home/user/Downloads/ags/Common/csrun.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/csrun.cpp.o"
+  "/home/user/Downloads/ags/Common/lzw.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/lzw.cpp.o"
+  "/home/user/Downloads/ags/Common/mousew32.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/mousew32.cpp.o"
+  "/home/user/Downloads/ags/Common/sprcache.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/__/Common/sprcache.cpp.o"
+  "/home/user/Downloads/ags/Engine/ac.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/ac.cpp.o"
+  "/home/user/Downloads/ags/Engine/acaudio.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acaudio.cpp.o"
+  "/home/user/Downloads/ags/Engine/acchars.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acchars.cpp.o"
+  "/home/user/Downloads/ags/Engine/acdialog.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acdialog.cpp.o"
+  "/home/user/Downloads/ags/Engine/acfonts.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acfonts.cpp.o"
+  "/home/user/Downloads/ags/Engine/acgfx.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acgfx.cpp.o"
+  "/home/user/Downloads/ags/Engine/acgui.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acgui.cpp.o"
+  "/home/user/Downloads/ags/Engine/acplatfm.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acplatfm.cpp.o"
+  "/home/user/Downloads/ags/Engine/acpllnx.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acpllnx.cpp.o"
+  "/home/user/Downloads/ags/Engine/acsound.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/acsound.cpp.o"
+  "/home/user/Downloads/ags/Engine/ali3dsw.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/ali3dsw.cpp.o"
+  "/home/user/Downloads/ags/Engine/bigend.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/bigend.cpp.o"
+  "/home/user/Downloads/ags/Engine/libsrc/hq2x/hq2x3x.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/libsrc/hq2x/hq2x3x.cpp.o"
+  "/home/user/Downloads/ags/Engine/misc.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/misc.cpp.o"
+  "/home/user/Downloads/ags/Engine/routefnd.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/routefnd.cpp.o"
+  "/home/user/Downloads/ags/Engine/scrptrt.cpp" "/home/user/Downloads/ags/Engine/CMakeFiles/ags.dir/scrptrt.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "THIS_IS_THE_ENGINE"
+  "LINUX_VERSION"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
